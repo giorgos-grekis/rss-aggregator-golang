b1714185cfb2becb002d0bd082f70765241f59e9
giorgos-grekis <georgrekis@gmail.com>
2024-02-19 20:30:42 +0200

    add cors

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/giorgos-grekis/rss-aggregator
 
 go 1.22.0
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi/v5 v5.0.12
+
+require github.com/go-chi/cors v1.2.1
--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
 
 	err := godotenv.Load()
 
@@ -17,7 +19,31 @@ func main() {
 		log.Fatal("PORT is not in the environment")
 	}
 
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		// AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"* "},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	portString := os.Getenv("PORT")
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println("PORT:", portString)
 }
